bbd1c188ebbf2339a5b55d5f0f1c950d31592d75
w1llyVR <wvaleric@gmail.com>
2023-04-28 15:09:36 -0500

    Snake_Miguel_WIlly

--- snake.js
+++ snake.js
@@ -1,33 +1,73 @@
 // Obtener el objeto canvas
 var canvas = document.getElementById("myCanvas");
-
-// Obtener el contexto del canvas
+var puntos
+    // Obtener el contexto del canvas
 var ctx = canvas.getContext("2d");
 
 // Posición inicial del rectángulo
 var rectX = canvas.width / 2;
 var rectY = canvas.height / 2;
 
+var desX = 10
+var desY = 0
+var vivo = true
+
+var puntosActuales = 0
+let apple = {
+    posX: 0,
+    posY: 0
+}
+
 // Función para dibujar el rectángulo en una posición
 function drawRect() {
-    ctx.fillStyle = "#FF0000";
+    ctx.fillStyle = "#008000";
     ctx.fillRect(rectX, rectY, 10, 10);
 }
 
+function drawApple() {
+    ctx.fillStyle = "#ff0000";
+    ctx.fillRect(apple.posX, apple.posY, 10, 10);
+}
+
+function updateRect() {
+    rectX += desX
+    rectY += desY
+    if (rectX < 0 || rectX > canvas.width || rectY < 0 || rectY > canvas.height)
+        vivo = false
+    if (rectX == apple.posX && rectY == apple.posY)
+        puntos += 10
+    if (vivo == false) {
+        clearInterval(intervalo)
+        location.reload()
+    }
+
+}
 // Función para actualizar la posición del rectángulo en cada cuadro de animación
-function updateRect(event) {
+function updateDirection(event) {
     switch (event.keyCode) {
         case 65: // Izquierda
-            rectX -= 5;
+            if (desX == 0) {
+                desX = -10;
+                desY = 0
+            }
             break;
         case 87: // Arriba
-            rectY -= 5;
+            if (desY == 0) {
+                desY = -10;
+                desX = 0;
+            }
             break;
         case 68: // Derecha
-            rectX += 5;
+            if (desX == 0) {
+                desX = 10;
+                desY = 0
+            }
             break;
         case 83: // Abajo
-            rectY += 5;
+            if (desY == 0) {
+                desY = 10;
+                desX = 0;
+            }
             break;
     }
 }
@@ -36,10 +76,12 @@ function updateRect(event) {
 function animate() {
     // Limpiar el canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    updateRect();
     // Dibujar el rectángulo en la nueva posición
     drawRect();
+    drawApple();
 }
-window.addEventListener("keydown", updateRect);
+window.addEventListener("keydown", updateDirection);
 // Comenzar la animación
-setInterval(animate, 10);
+
+const intervalo = setInterval(animate, 200);
